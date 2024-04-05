f8ecc0d15731234ca8d3f0c998f92ce923f2fd23
Ivy <ivyjkemboi@gmail.com>
2024-04-05 13:28:40 +0300

    map

--- index.js
+++ index.js
@@ -11,6 +11,46 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+    //get each array element i.e each sentence and use variable 'tutorial' to access it
+  return  tutorials.map(function(tutorial){
+    //now that we have each array item, split each sentence into each word. split where you find space i.e (" ")
+    const originalText = tutorial.split(" ");
+
+    const eachString = originalText.map(function(originalText){
+      return originalText.charAt(0).toUpperCase() + originalText.slice(1);}).join (" ");
+
+      return eachString;
+    });
+
 }
+
+console.log (titleCased);
+
+// const eachTutorial = tutorials.map(function(tutorial){
+//   const originalText = tutorial.split(" ");
+//   const eachSentence = originalText;
+//   const eachWord = eachSentence.map(function(eachWord){
+//         //console.log(eachWord);    
+//     const titleCase = eachWord.charAt(0).toUpperCase()
+// + eachWord.slice(1)
+// console.log (titleCase);
+// return tutorial;
+// });
+
+// });
+
+// const splitText = tutorials.split(" ");
+// const oneWord = splitText[1];
+// console.log (oneWord);
+
+
+// function titleCase(str) {
+//   return str.toLowerCase().split(' ').map(function(word) {
+//     return word.replace(word[0], word[0].toUpperCase());
+//   }).join(' ');
+// }
+
+// const titleCased = tutorials.map((tutorial) => titleCase(tutorial));
+// console.log(titleCased);
+//const rollCall = skiSchool.map((student) => student + " the skier");
